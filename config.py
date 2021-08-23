f9b97941c6e9582a8fc812befc522bbebd7eb9a1
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-08-23 16:26:06 +0300

    edit config.py

--- config.py
+++ config.py
@@ -1,17 +1,28 @@
+import os
+
 class Config:
     
     SECRET_KEY = 'global_football'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oscar:123456789@localhost/global_football'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
 
 class DevConfig(Config):
 
     DEBUG = True
+    ENV = 'development'
 
 
 class ProdConfig(Config):
     pass
+
+
 
 
 config_options = {'development': DevConfig, 'production': ProdConfig}
