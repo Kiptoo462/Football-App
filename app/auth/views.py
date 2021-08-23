8643919c7583c36ec1834810ef188c03e2068571
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-08-23 16:08:47 +0300

    Html setup for authentication

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,11 +6,14 @@ from . import auth
 from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
+from ..email import welcome_message
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm()
+
     if form.validate_on_submit():
+
         user = User(
                     email=form.email.data,
                     username=form.username.data,
@@ -18,30 +21,38 @@ def signup():
                     
         db.session.add(user)
         db.session.commit()
+        welcome_message("Welcome to the Global Football community.","email/welcome",user.email,user=user)
+
         return redirect(url_for('auth.login'))
 
     title = "Sign Up"
-    return render_template('auth/signup.html', form=form, title=title)
+    return render_template('auth/signup.html', RegistrationForm=form, title=title)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
+
         user = User.query.filter_by(email=form.email.data).first()
+
         if user is not None and user.passwordVerification(
                 form.password.data):
-            login_user(user, form.remember.data)
+            login_user(user,form.remember.data)
+
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Authentication Failed!')
+
     title = "Login"
-    return render_template('auth/login.html', form=form, title=title)
+    return render_template('auth/login.html', LoginForm=form, title=title)
 
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
+
     return redirect(url_for('main.index'))
 
 
